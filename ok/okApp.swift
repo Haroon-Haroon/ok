2f89d76919c418c16529230ba6a22948ecde8e2c
Hamad-Haroon <hamad.732@dubai-sms.com>
2023-06-24 14:47:48 +0400

    Initial Commit

---
+++ ok/okApp.swift
@@ -0,0 +1,17 @@
+//
+//  okApp.swift
+//  ok
+//
+//  Created by Hamad H on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct okApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
